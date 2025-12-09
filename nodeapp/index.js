428c86de375b4d9d9bd8c0f642986303e6ab72b9
thavishiminolka <155363547+thavishiminolka@users.noreply.github.com>
2025-12-10 00:10:30 +0530

    new

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -7,7 +7,7 @@ app.get("/", function (req, res) {
 
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Appp listening on port 3000!");
+  console.log("App listening on port 3000!");
 });
 
 module.exports = app;
